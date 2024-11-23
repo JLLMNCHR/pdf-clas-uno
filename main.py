d52bb5d7f6e06796abf703c8ca5c0f6ef6ec2a75
JLLMNCHR <jllmnchr@gmail.com>
2024-11-23 18:42:07 +0100

    Cambio a unidad D

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 start_time = time.time()
 
 extractor = PDFExtractor()
-extractor.extract_text_to_file("C:/PRUEBAS_PDF/ENTRADA", "C:/PRUEBAS_PDF/SALIDA")
+extractor.extract_text_to_file("D:/PRUEBAS_PDF/ENTRADA", "D:/PRUEBAS_PDF/SALIDA")
 
 end_time = time.time()
 execution_time = end_time - start_time
