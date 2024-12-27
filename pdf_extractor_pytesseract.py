b961d71c8ae2b6bdb1aaa5f404523482c1138595
jl.lominchar <jose.lominchar@tirea.es>
2024-12-27 09:52:11 +0100

    Más info en consola y log

--- pdf_extractor_pytesseract.py
+++ pdf_extractor_pytesseract.py
@@ -14,15 +14,19 @@ logging.basicConfig(
 # Declaración de contadores globales
 ok_files = 0
 ko_files = 0
+total_files = 0
 
 def extract_text_from_pdf(input_path, output_path, processed_folder):
     """
     Extrae texto de un archivo PDF utilizando OCR si es necesario.
     Mueve el PDF procesado a la carpeta PROCESADOS o a la carpeta ERROR si falla.
     """
-    print(".", end='', flush=True)
+    global ok_files, ko_files, total_files
+
+    total_files += 1
+    nom_fich = os.path.basename(input_path)
+    print(f" .{total_files}-{nom_fich}", end='', flush=True)
     
-    global ok_files, ko_files
     error_folder = os.path.join(os.path.dirname(output_path), 'ERROR')    
     
     try:
@@ -44,10 +48,10 @@ def extract_text_from_pdf(input_path, output_path, processed_folder):
         if not text.strip():
             print("!", end='', flush=True)
             ko_files += 1
-            logging.error(f"PDF sin contenido: {input_path}")
+            logging.error(f"{nom_fich} sin contenido")
             if not os.path.exists(error_folder):
                 os.makedirs(error_folder)
-            os.rename(input_path, os.path.join(error_folder, os.path.basename(input_path)))
+            os.rename(input_path, os.path.join(error_folder, nom_fich))
             return
 
         # Guarda el texto extraído solo si hay contenido
@@ -57,15 +61,15 @@ def extract_text_from_pdf(input_path, output_path, processed_folder):
         # Mueve el PDF procesado a la carpeta PROCESADOS
         print(":", end='', flush=True)
         ok_files += 1
-        os.rename(input_path, os.path.join(processed_folder, os.path.basename(input_path)))
+        os.rename(input_path, os.path.join(processed_folder, nom_fich))
 
     except Exception as e:
         print("X", end='', flush=True)
         ko_files += 1
-        logging.error(f"Error procesando {input_path}: {e}")
+        logging.error(f"{nom_fich} Error: {e}")
         if not os.path.exists(error_folder):
             os.makedirs(error_folder)
-        os.rename(input_path, os.path.join(error_folder, os.path.basename(input_path)))
+        os.rename(input_path, os.path.join(error_folder, nom_fich))
 
 def process_pdfs(input_folder, output_folder, max_workers=1):
     """Procesa todos los PDFs, mueve los procesados a PROCESADOS y los que fallan a ERROR."""
@@ -78,7 +82,6 @@ def process_pdfs(input_folder, output_folder, max_workers=1):
 
     pdf_files = [f for f in os.listdir(input_folder) if f.lower().endswith('.pdf')]
     #total_files = len(pdf_files)
-    total_files = 0
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = {
@@ -94,9 +97,10 @@ def process_pdfs(input_folder, output_folder, max_workers=1):
             pdf_file = futures[future]
             try:
                 future.result()
-                total_files += 1
+                # total_files += 1
             except Exception as e:
-                logging.error(f"Error procesando {pdf_file}: {e}")
+                nom_fich = os.path.basename(pdf_file)
+                logging.error(f"{nom_fich} Excepción {e}")
 
     print("\n");
     print(f"Total de archivos: {total_files}")
@@ -121,7 +125,8 @@ def ocr_page(pdf_path, page_num, ocr_lang='spa'):
             return ocr_text
     except Exception as e:
         print("x", end='', flush=True)
-        logging.error(f"Error al aplicar OCR a la página {page_num}: {e}")
+        nom_fich = os.path.basename(pdf_path)
+        logging.error(f"{nom_fich} Error al aplicar OCR a la página {page_num}: {e}")
     return ""
 
 if __name__ == "__main__":
